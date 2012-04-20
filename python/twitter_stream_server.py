ec54f51441169267aae2ebb7d9ba45e319ae3a85
Eddy Parris <eddy@communecation.net>
2012-04-20 12:46:37 +0100

    Attempt to solve the mysterious 102 tweet issue. Their seems to be a problem where the data sent is ofter '102', attempted to solve this with some different (better?) threading

--- python/twitter_stream_server.py
+++ python/twitter_stream_server.py
@@ -4,7 +4,7 @@ import sys
 import signal
 import time
 from pprint import pprint
-from threading import Timer
+import threading
 
 import tweepy
 import tweepy_helpers
@@ -12,38 +12,62 @@ import tweepy_helpers
 import pusher
 
 count = 0
+config = ""
+
+class Sender(threading.Thread):
+    def __init__(self, seconds):
+        threading.Thread.__init__(self)
+        print "Preparing to send data"
+        self.kill_received = False 
+        self.seconds = seconds
+        self.p = pusher.Pusher()
+        
+    def run(self):
+        while not self.kill_received:
+            self.task()
+    
+    def task(self):
+        global count
+        try:
+            print "Sending: " + str(count)
+            self.p['my-channel'].trigger('my-event', {'count': count})
+        except Exception,e:
+            print e
+
+        count = 0
+        time.sleep(self.seconds)
+
 
 def handle_data(data):
     global count
-    
+    count = count + 1
+
+def main(args):
+    global config
     try:
-        count = count + 1
-    except Exception,e:
-        print e
+        config_path = os.path.dirname(sys.argv[0]) + "/config.json"
+        print "Getting config from %s" % config_path
+        config = json.loads(open(config_path, 'r').read())
+        
+        # prepare Pusher object
+        pusher.app_id = config['pusher']['app_id'].encode('ascii')
+        pusher.key = config['pusher']['key'].encode('ascii')
+        pusher.secret = config['pusher']['secret'].encode('ascii')
+        
+        print "Firing up threads"
+        threads = []
+        s = Sender(1.0)
+        threads.append(s)
+        s.start()
+        
+        print "Recording filter %s..." % config['search_terms']
+        tweepy_helpers.stream('filter', config, handle_data)
+        while True: time.sleep(1)
+        
+    except (KeyboardInterrupt, SystemExit):
+        print "Ctrl-c received! Sending kill to threads..."
+        for t in threads:
+            t.kill_received = True
 
-def send_data():
-    global count
-    try:
-        print "Sending: " + str(count)
-        p['my-channel'].trigger('my-event', {'count': count})
-    except Exception,e:
-        print e
-
-    count = 0
-
-    Timer(1.0, send_data).start()
-
-config_path = os.path.dirname(sys.argv[0]) + "/config.json"
-# config_path = "config.json"
-print "Getting config from %s" % config_path
-config = json.loads(open(config_path, 'r').read())
-
-# prepare Pusher object
-pusher.app_id = config['pusher']['app_id'].encode('ascii')
-pusher.key = config['pusher']['key'].encode('ascii')
-pusher.secret = config['pusher']['secret'].encode('ascii')
-p = pusher.Pusher()
-
-print "Recording filter %s..." % config['search_terms']
-thread = Timer(1.0, send_data).start()
-tweepy_helpers.stream('filter', config, handle_data)
+if __name__ == '__main__':
+  main(sys.argv)
